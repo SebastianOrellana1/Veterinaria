91693c64ecbb7018e995cfd84cd5da5588939aa1
SebastianAndres1 <38734325+SebastianAndres1@users.noreply.github.com>
2019-06-10 14:42:35 -0400

    hola
    eliminar

--- conexion.js
+++ conexion.js
@@ -75,17 +75,84 @@ function guardar() {
 
 }
 //leer datos
-var tabla = document.getElementById('tabla');
-db.collection("cliente").onSnapshot((querySnapshot) => {
-    tabla.innerHTML = "";
-    querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-        tabla.innerHTML += `
-        <tr>
-            <td>${doc.data().nombre}</td>
-            <td>${doc.data().apellido}</td>
-            <td>${doc.data().email}</td>
-        </tr>
-        `
+function buscar() {
+    var tabla = document.getElementById('tabla');
+    var pass1 = document.getElementById('password1').value;
+    var eml1 = document.getElementById('email1').value;
+    db.collection("cliente").onSnapshot((querySnapshot) => {
+        tabla.innerHTML = "";
+        querySnapshot.forEach((doc) => {
+            console.log(`${doc.id} => ${doc.data()}`);
+            if (eml1 == `${doc.data().email}` && pass1 == `${doc.data().contraseña}`) {
+                tabla.innerHTML += `
+            <tr>
+                <td>${doc.data().nombre}</td>
+                <td>${doc.data().apellido}</td>
+                <td>${doc.data().email}</td>
+                <td><button onclick="muestraInput()" class="btn waves-effect teal darken-1">Modificar</button></td>
+            </tr>
+            `
+            }
+        });
     });
-});
+}
+function muestraInput() {
+    var muestra = document.getElementById('muestra');
+    muestra.innerHTML += `
+    <div class="row">
+        <div class="input-field col s6">
+            <input id="nombre" type="text" class="validate white-text">
+            <label for="first_name" class="white-text">Nombre</label>
+        </div>
+        <div class="input-field col s6">
+            <input id="apellido" type="text" class="validate white-text">
+            <label for="last_name" class="white-text">Apellido</label>
+        </div>
+    </div>
+    <div class="row">
+        <div class="input-field col s6">
+            <input id="email1" type="email" class="validate white-text">
+            <label for="email" class="white-text">Email</label>
+        </div>
+        <div class="input-field col s6">
+            <input id="password1" type="password" class="validate white-text">
+            <label for="password" class="white-text">Password</label>
+        </div>
+    </div>
+    <button onclick="" class="btn waves-effect teal darken-1">Modificar</button>
+    `
+}
+
+
+//eliminar
+
+function buscar1() {
+    var tabla = document.getElementById('tabla2');
+    var pass2 = document.getElementById('password2').value;
+    var eml2 = document.getElementById('email2').value;
+    db.collection("cliente").onSnapshot((querySnapshot) => {
+        tabla.innerHTML = "";
+        querySnapshot.forEach((doc) => {
+            console.log(`${doc.id} => ${doc.data()}`);
+            if (eml2 == `${doc.data().email}` && pass2 == `${doc.data().contraseña}`) {
+                tabla.innerHTML += `
+            <tr>
+                <td>${doc.data().nombre}</td>
+                <td>${doc.data().apellido}</td>
+                <td>${doc.data().email}</td>
+                <td>  <button onclick="eliminar('${doc.id}')" class="btn waves-effect teal darken-1">Eliminar</button> </td>
+            </tr>
+            `
+            }
+        });
+    });
+}
+
+//borrar documentos
+function eliminar(id){
+    db.collection("cliente").doc(id).delete().then(function() {
+        console.log("Document successfully deleted!");
+    }).catch(function(error) {
+        console.error("Error removing document: ", error);
+    });
+}
